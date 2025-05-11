6f8e5a2a8bd6e12b5206815fb8d6a8ec098389eb
sahilbajare25 <68180439+sahilbajare25@users.noreply.github.com>
2025-05-11 11:43:16 +0530

    Delete Assignment1_4.py

--- Assignment1_4.py
+++
@@ -1,8 +0,0 @@
-def main():                     # here main function body is written 
-    i = 0                       # here one local variable is 'i' taken and store with zero value for counter
-    while i<5:                  # here while loop is written which check whether local variable 'i' is less than 5 or not
-        print("Marvellous")     # here if condition return True then code gets inside while loop and print "Marvellous"
-        i = i + 1               # here local variable 'i' which is used as counter gets incrementd by 1 
-
-if __name__ == "__main__":      # code start from here
-    main()                      # here main function get call and code jump to line 1
