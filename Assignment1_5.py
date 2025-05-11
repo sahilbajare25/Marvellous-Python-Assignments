1607df83d18eafe18f2bc1674c51ac1310592a9f
sahilbajare25 <68180439+sahilbajare25@users.noreply.github.com>
2025-05-11 11:43:32 +0530

    Delete Assignment1_5.py

--- Assignment1_5.py
+++
@@ -1,7 +0,0 @@
-def main():                         # here main function body is written
-    for no in range(10,0,-1):       # here for loop is used to retrive data from range function which start with 10 and decrement 1 on every loop until no reach 0 and for loop stop
-        print(no, end=" ")          # here data from no is print with end=" " written to print all values on same line
-
-
-if __name__ == "__main__":          # code start from here
-    main()                          # main function get call from here and code jump to line 1
