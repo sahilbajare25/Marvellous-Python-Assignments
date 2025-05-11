d48bea39bdc65f700280407c5543b11fb54dfd75
sahilbajare25 <68180439+sahilbajare25@users.noreply.github.com>
2025-05-11 11:42:37 +0530

    Delete Assignment1_10.py

--- Assignment1_10.py
+++
@@ -1,8 +0,0 @@
-def main():                         # here main function body is written
-    print("Enter your name : ")     # here code ask for user input
-    Name = input()                  # here input provided is store into Name variable
-
-    print(len(Name))                #here len function is used to print the length of data store in Name variable
-
-if __name__ == "__main__":          # code start from here
-    main()                          # main function get call from here and code jump to line 1
